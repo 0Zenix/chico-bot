321d21d5efc1fc68a2d5cad58bdd8b3c6e307fb2
Lucas Bueno <lucasbno@proton.me>
2024-04-21 20:41:56 -0300

    initial commit

---
+++ formulario.js
@@ -0,0 +1,39 @@
+const formularioPerfil = [
+  {
+    PerguntaTempo:
+      "Por quanto tempo você planeja manter seus investimentos? Curto prazo (menos de 3 anos), médio prazo (3 a 5 anos), ou longo prazo (mais de 5 anos)?",
+    resposta1: "Longo prazo", // Agressivo
+    resposta2: "Curto prazo", // Conservador
+    resposta3: "Médio prazo", // Moderado
+  },
+  {
+    perguntaObjetivo: " ",
+    resposta1: " ",
+    resposta2: " ",
+    resposta3: " ",
+  },
+  {
+    perguntaObjetivo: " ",
+    resposta1: " ",
+    resposta2: " ",
+    resposta3: " ",
+  },
+  {
+    perguntaObjetivo: " ",
+    resposta1: " ",
+    resposta2: " ",
+    resposta3: " ",
+  },
+  {
+    perguntaObjetivo: " ",
+    resposta1: " ",
+    resposta2: " ",
+    resposta3: " ",
+  },
+  {
+    perguntaObjetivo: " ",
+    resposta1: " ",
+    resposta2: " ",
+    resposta3: " ",
+  },
+]
