9e3527ceff8e9994a376f054e7dfca9f7d0a78fa
Allan <162365080+Allannll@users.noreply.github.com>
2024-04-25 19:56:41 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -2,38 +2,38 @@ const formularioPerfil = [
     {
       Pergunta:
         "Por quanto tempo você planeja manter seus investimentos? Curto prazo (menos de 3 anos), médio prazo (3 a 5 anos), ou longo prazo (mais de 5 anos)?",
-      resposta1: "Longo prazo", // Agressivo
-      resposta2: "Curto prazo", // Conservador
-      resposta3: "Médio prazo", // Moderado
+      resposta1: "A- Longo prazo", // Agressivo
+      resposta2: "B- Curto prazo", // Conservador
+      resposta3: "C- Médio prazo", // Moderado
     },
     {
       pergunta: "Qual seu objetivo com os rendimentos?",
-      resposta1: "Não dependo dos rendimentos", //Agressivo
-      resposta2: "Usar eventualmente", // Moderado
-      resposta3: "Usar para honrar alguns compromissos regularmente", // Conservador
+      resposta1: "A- Não dependo dos rendimentos", //Agressivo
+      resposta2: "B- Usar eventualmente", // Moderado
+      resposta3: "C- Usar para honrar alguns compromissos regularmente", // Conservador
     },
     {
       pergunta: "Já possui algum valor investido?",
-      resposta1: "Entre R$50.000 e R$100.000", // Moderado
-      resposta2: "Até R$50.000", // Conservador
-      resposta3: "Mais que R$100.000", // Agressivo
+      resposta1: "A- Entre R$50.000 e R$100.000", // Moderado
+      resposta2: "B- Até R$50.000", // Conservador
+      resposta3: "C- Mais que R$100.000", // Agressivo
     },
     {
       pergunta: "Qual a principal finalidade dos seus investimentos?",
-      resposta1: " Preservar o patrimônio sem grandes riscos", //Conservador
-      resposta2: " Maximizar o potencial de ganho assumindo um risco maior", // AgressivO
-      resposta3: "Uma combinação entre preservação do patrimônio e sua valorização", // Moderado
+      resposta1: "A- Preservar o patrimônio sem grandes riscos", //Conservador
+      resposta2: "B- Maximizar o potencial de ganho assumindo um risco maior", // AgressivO
+      resposta3: "C- Uma combinação entre preservação do patrimônio e sua valorização", // Moderado
     },
     {
       pergunta: "Como você classifica sua formação e experiência no mercado financeiro?",
-      resposta1: "Não possuo formação na área e conheço pouco sobre o mercado financeiro", // Conservador
-      resposta2: "Possuo formação na área e/ou tenho certo conhecimento sobre alguns conceitos.", // Moderado
-      resposta3: "Possuo formação ou tenho domínio sobre o mercado financeiro.", // Agressivo
+      resposta1: "A- Não possuo formação na área e conheço pouco sobre o mercado financeiro", // Conservador
+      resposta2: "B- Possuo formação na área e/ou tenho certo conhecimento sobre alguns conceitos.", // Moderado
+      resposta3: "C- Possuo formação ou tenho domínio sobre o mercado financeiro.", // Agressivo
     },
     {
       pergunta: "Qual o seu grau de interesse em assuntos ligados à Economia e ao Mercado Financeiro?",
-      resposta1: "Nenhum interesse", // Conservador
-      resposta2: "Muito interesse", // Agressivo
-      resposta3: "Conhecimentos prévios", // Moderado
+      resposta1: "A- Nenhum interesse", // Conservador
+      resposta2: "B- Muito interesse", // Agressivo
+      resposta3: "C- Conhecimentos prévios", // Moderado
     },
   ]
